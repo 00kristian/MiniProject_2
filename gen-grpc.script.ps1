224b6ea49fe5f9d41d52452c13dbd3ba1e9c75b6
ehou <ehou@itu.dk>
2021-10-26 11:15:09 +0200

    genereated pb and grpcgo file

---
+++ gen-grpc.script.ps1
@@ -0,0 +1,1 @@
+protoc --proto_path=. --go_out=. --go-grpc_out=. --go_opt=paths=source_relative proto/chittychat.proto
---
+++ server/server.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+func main(){
+	lis, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalf("Failed to listen on port 8080: %v", err)
+	}
+
+	server := grpc.NewServer()
+
+	if err := server.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve on port 8080: %v", err)
+	}
+}
