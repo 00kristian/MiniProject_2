3259c65f6aa7628288b3c7e24da20e11061d2d27
Emil Houlborg <82398520+emiloh@users.noreply.github.com>
2021-11-02 12:37:25 +0100

    Fully implemented (#1)
    * Working chittychat
    
    * added comment
    
    * Working with publish
    
    * Leave works - only missing lamport
    
    * Leave implemented - missing lamport
    
    * Lamport implemented
    
    Co-authored-by: ehou <ehou@itu.dk>

--- client/client.go
+++ client/client.go
@@ -1,32 +1,200 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"log"
+	"os"
+	"strings"
+	"sync"
 
-	"github.com/00kristian/MiniProject_2/chittychat"
+	"github.com/00kristian/MiniProject_2/proto"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 )
 
+// Global variable for our client
+var client proto.ChatClient
+// Global wait group
+var wait *sync.WaitGroup
+// Mutex for locking
+var mu sync.Mutex
+
+// lamport time for given client
+var lamport uint64 = 0
+
+// Init func to initialize the wait group
+func init(){
+	wait = &sync.WaitGroup{}
+}
+
+// Lets the client join into the server
+func join(id string, name string) error {
+	// Stream error to be used if something fails
+	var sError error
+
+	user := &proto.User{
+		Id: id,
+		Name: name,
+		Active: true,
+	}
+
+	// join event increments lamport by one
+	mu.Lock()
+	lamport += 1
+	mu.Unlock()
+
+	joinMessage := &proto.Message{
+		Id: "",
+		Text: user.Name + " joined Chitty-Chat at Lamport time ",
+		Lamport: lamport,
+	}
+
+	// Creates the stream, that is return when a user joins the server
+	stream, err := client.Join(context.Background(), user)
+	// Publish the join message
+	_, joinMessageErr := client.Publish(context.Background(), joinMessage)
+
+	if err != nil {
+		log.Fatalf("Connection failed: %v", err)
+	}
+	if joinMessageErr != nil {
+		log.Fatalf("Error occured when publishing join message: %v", joinMessageErr)
+	}
+
+	// Increments the wait group by one
+	wait.Add(1)
+
+	// Go rotuine that spawns an anonymous function, that takes the newly created stream as input.
+	// The proto.Chat_JoinClient allows us to define the behaviour in order to implement the streaming part of our client
+	go func(str proto.Chat_JoinClient) {
+		// Decrements the wait group when mehtod exits
+		defer wait.Done()
+		
+		// Infinite for loop 
+		for{
+			// Wait until a message is recieved in the stream
+			msg, err := str.Recv()
+			mu.Lock()
+			lamport = max(lamport, msg.Lamport) + 1
+			mu.Unlock()
+
+			// If an error occurs, the goroutine and the for loop must terminate. 
+			// Error is passed to the local sError variable
+			if err != nil {
+				sError = fmt.Errorf("Error occured when reading message: %v", err)
+				break;
+			}
+			// If id == "", it is a join message
+			if msg.Id == "" {
+				log.Printf("[%s: %d] %s", user.Id, lamport, msg.Text)
+			}else{
+				log.Printf("[%s: %d] %s: %s", user.Id, lamport, msg.Id, msg.Text)
+			}
+		}
+	}(stream)
+
+	return sError
+}
+
 func main(){
+	// Reader to read user input
+	reader := bufio.NewReader(os.Stdin)
+
+	// Dummy channel to ensure all go routines are finished
+	done := make(chan int)
+
+	// Reads and parse name into id and name, which is used to connect
+	fmt.Print("Please enter you name: ")
+	temp, _ := reader.ReadString('\n')
+	name := strings.TrimSpace(temp)
+	id := name
+
+	// Connect to our server - no https, so connect with grpc.WithInsecure()
 	conn, err := grpc.Dial(":8080", grpc.WithInsecure())
 	if err != nil{
 		log.Fatalf("Could not connect: %s", err)
 	}
 
+	// When method is done close the connection
 	defer conn.Close()
 
-	c := chittychat.NewChittyChatClient(conn)
+	// Creates the client on our connection
+	client = proto.NewChatClient(conn)
+	
+	// Show welcome message
+	welcome()
 
-	message := chittychat.Message{
-		Text: "Hello from client",
+	// Join the server with the given name and id
+	join(id, name)
+	
+	//Increment wait gorup before go routine
+	wait.Add(1)
+
+	// Go routine that spawns an anonymous function
+	go func(){
+		defer wait.Done()
+
+		// Create scanner in order to scan user messages
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan(){
+			mu.Lock()
+			lamport += 1
+			mu.Unlock()
+			msg := &proto.Message{
+				Id: id,
+				Text: scanner.Text(),
+				Lamport: lamport,
+			}
+			// Check if said message is a command
+			if strings.Contains(msg.Text, "\\leave"){
+				_ , errLeave := client.Leave(context.Background(), &proto.Id{Id: msg.Id, Lamport: msg.Lamport})
+				if errLeave != nil{
+					log.Fatalf("Error occured when trying to leave: %v", errLeave)
+				}
+				wait.Done()
+				break
+			} else if strings.Contains(msg.Text, "\\help"){
+				fmt.Println("------------------------------------")
+				fmt.Println("Following commands are available:")
+				fmt.Println("\\leave - Exits Chitty-Chat.")
+				fmt.Println("\\help - Shows this menu again.")
+				fmt.Println("------------------------------------")
+			} else{
+				// Call the broadcast message and distibute the message through all active useres
+				_, err := client.Publish(context.Background(), msg)
+				if err != nil {
+					log.Fatalf("Error sending message: %v", err)
+					break
+				}
+			}
+		}
+	}()
+	
+	// Go routine that spawns anonymous function that ensures that the wait group waits for the go routines to exit
+	go func(){
+		wait.Wait()
+		// Closes our done dummy channel
+		close(done)
+	}()
+
+	// Acts as a blocker - code will not proceed from this until our done channel has been closed. That happens after all our go routines are done.
+	<- done
+}
+
+func welcome(){
+	fmt.Println("Welcome to Chitty-chat! =^.^=")
+	fmt.Println("------------------------------------")
+	fmt.Println("Following commands are available:")
+	fmt.Println("\\leave - Exits Chitty-Chat.")
+	fmt.Println("\\help - Shows this menu again.")
+	fmt.Println("------------------------------------")
+}
+
+func max(x, y uint64) uint64{
+	if x >= y{
+		return x
+	}else {
+		return y
 	}
-	response, err := c.Publish(context.Background(), &message) 
-
-	if err != nil{
-		log.Fatalf("Error when calling publish: %s", err)
-	}
-
-	log.Printf("Response from server: %s", response)
-
 }
--- server/server.go
+++ server/server.go
@@ -1,25 +1,203 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net"
-	"github.com/00kristian/MiniProject_2/chittychat"
+	"sync"
+
+	"github.com/00kristian/MiniProject_2/proto"
 	"google.golang.org/grpc"
 )
 
-func main(){
-	lis, err := net.Listen("tcp", ":8080")
-	if err != nil {
-		log.Fatalf("Failed to listen on port 8080: %v", err)
+// Mutex for locking lamport
+var mu sync.Mutex
+
+// Lamport time for server
+var lamport uint64 = 0
+
+type Connection struct {
+	stream proto.Chat_JoinServer
+	user *proto.User
+	error chan error 
+}
+
+type Server struct {
+	// Has to be implemented, otherwise the grpc cannot register
+	proto.UnimplementedChatServer
+	connections map[string]*Connection
+}
+
+func (s *Server) Leave(ctx context.Context, Id *proto.Id) (*proto.Empty, error){
+	mu.Lock()
+	lamport = max(lamport, Id.Lamport) + 1
+	mu.Unlock()
+	temp := s.connections[Id.Id]
+	temp.user.Active = false
+	s.connections[Id.Id] = temp
+	leaveMessage := &proto.Message{
+		Id: "",
+		Text: Id.Id + " left Chitty-Chat at Lamport time " + fmt.Sprintf("%d", lamport),
+	}
+	s.Broadcast(ctx, leaveMessage)
+	return &proto.Empty{}, nil
+}
+
+// Implementation of the Publish rpc - Allows users to publish messages to be broadcasted
+func (s *Server) Publish(ctx context.Context, msg *proto.Message) (*proto.Empty,error){
+	mu.Lock()
+	lamport = max(lamport, msg.Lamport) + 1
+	mu.Unlock()
+	
+	// if id == "", it is a join or leave message
+	if msg.Id == ""{
+		updatedMsg := &proto.Message{
+			Id: msg.Id,
+			Text: msg.Text + fmt.Sprintf("%d", lamport),
+			Lamport: lamport,
+		}
+		log.Printf("[Server: %d] A message was published with following content: %s", lamport, updatedMsg.Text)
+		s.Broadcast(ctx, updatedMsg)
+	}else{
+		log.Printf("[Server: %d] A message was published by %s with following content: %s", lamport, msg.Id, msg.Text)
+		s.Broadcast(ctx, msg)
+	}
+
+	return &proto.Empty{}, nil
+}
+
+// Implementation of the Join rpc - alllows user to join the server
+func (s *Server) Join(user *proto.User, stream proto.Chat_JoinServer) error{
+	// Create a connection to server	
+	conn := &Connection{
+		stream: stream,
+		user: user,
+		error: make(chan error),
 	}
 	
-	s := chittychat.Server{}
-	server := grpc.NewServer()
+	// Make the user active
+	conn.user.Active = true
 
-	chittychat.RegisterChittyChatServer(server, &s)
+	// Add the connection to the map of connections
+	s.connections[conn.user.Id] = conn
+
+	// Return whatever error that is in the conn error field
+	return <- conn.error
+}
+
+func (s *Server) Broadcast(ctx context.Context, msg *proto.Message) (*proto.Empty, error){
+	// Allows counting of go routines. Go routines can be added to the wait group, and it is possible to decrease the counter when a go routine finishes its job.
+	// This makes it possible to block the method from exiting before all go routines finishes their job.
+	wait := sync.WaitGroup{}
+
+	// Dummy channel for us to know when our all our go routines are done
+	done := make(chan int)
+
+	// Channel to pick up id's of active users
+	//aUsers := make(chan string, 10)
+	log.Printf("[Server: %d] Broadcasting message to active users:", lamport)
+	//Loop through all connections
+	for name, conn := range s.connections {
+		//Increments the counter of the wait group - increments by one for each connection
+		wait.Add(1)
+		// Gather active users
+		
+		// Go routine that spawn an anonymous function
+		go func (msg *proto.Message, conn *Connection){
+			// When the method exits, decrement the wait group by one
+			defer wait.Done()
+			
+			
+			// Check if user is active, and act if the user is
+			if conn.user.Active {
+				mu.Lock()
+				lamport += 1
+				updatedMsg := &proto.Message{
+					Id: msg.Id,
+					Text: msg.Text,
+					Lamport: lamport,
+				}
+				mu.Unlock()
+				log.Printf("[Server: %d] Sending message to %s.", lamport, conn.user.Id)
+				// Send message to the client which is attached to given connection
+				err := conn.stream.Send(updatedMsg)
+				
+				//aUsers <- conn.user.Id
+				
+				// If an error occurs - print the error and terminate the conneciton making the user go offline
+				if err != nil {
+					log.Fatalf("Error sending message %s - Error: %v", name, err)
+					conn.user.Active = false
+					// Pass the error to the error chan for the connection
+					conn.error <- err
+				}
+			}
+		}(msg, conn)
+	}
+
+	// Go routine that spawns anonymous function that ensures that the wait group waits for the go routines to exit
+	go func(){
+		wait.Wait()
+		// Closes our active user channel in order to loop over it
+		//close(aUsers)
+		// Closes our done dummy channel
+		close(done)
+	}()
+
+	// Create logstring to be printed for a message to be broadcasted
 	
+	// logString := "" 
+	// if msg.Id == ""{
+	// 	logString = "Broadcasting message to active users:"
+	// }else{
+	// 	logString = fmt.Sprintf("Broadcasting %s's message to active users:", msg.Id)
+	// }
+	// for id := range aUsers{
+	// 	logString += " " + id
+	// }
+	// log.Print(logString + "\n\n")
 
-	if err := server.Serve(lis); err != nil {
-		log.Fatalf("Failed to serve on port 8080: %v", err)
+	// Acts as a blocker - code will not proceed from this until our done channel has been closed. That happens after all our go routines are done.
+	<- done
+	
+	// Method can exit, and nothing with no error
+	return &proto.Empty{}, nil
+}
+
+
+func main(){
+	// Create the map of connections
+	connections := make(map[string]*Connection)
+
+	// Reference to our server with our connections
+	server := &Server{proto.UnimplementedChatServer{},connections}
+
+	// Startup of the grpc server
+	grpcServer := grpc.NewServer()
+
+	// Create a listener. This listener listen on our port 8080
+	listener, err := net.Listen("tcp", ":8080")
+
+	//Check if error occured  when trying to listen on port
+	if err != nil {
+		log.Fatalf("Error creating server: %v", err)
+	}
+
+	//Print to show that server has started
+	log.Printf("[Server: %d] Started server on port: 8080", lamport)
+
+	// Register our Chat server on out grpc server, and pass our service which is the server type
+	proto.RegisterChatServer(grpcServer, server)
+
+	// Serve incomming connetions to the listener
+	grpcServer.Serve(listener)
+}
+
+func max(x, y uint64) uint64{
+	if x >= y{
+		return x
+	}else {
+		return y
 	}
 }
